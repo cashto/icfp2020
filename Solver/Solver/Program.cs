13ca232c2f7d4f5b46353abca257595e8fde1ae0
cashto <cashto@gmail.com>
2020-07-17 22:30:36 -0700

    Send a different string

--- Solver/Solver/Program.cs
+++ Solver/Solver/Program.cs
@@ -10,13 +10,42 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Net.WebSockets;
 using System.Net.Http;
+using System.Net.Mime;
+using System.Threading.Tasks;
 
 namespace Solver
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
+            var serverUrl = args[0];
+            var playerKey = args[1];
+            Console.WriteLine($"ServerUrl: {serverUrl}; PlayerKey: {playerKey}");
+
+            if (!Uri.TryCreate(serverUrl, UriKind.Absolute, out var serverUri))
+            {
+                Console.WriteLine("Failed to parse ServerUrl");
+                return 1;
+            }
+
+            using var httpClient = new HttpClient { BaseAddress = serverUri };
+            var requestContent = new StringContent(playerKey, Encoding.UTF8, MediaTypeNames.Text.Plain);
+            using var response = await httpClient.PostAsync("", requestContent);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Unexpected server response: {response}");
+                return 2;
+            }
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Server response: {responseString}");
+
+            return 0;
+        }
+
+        static void DeadCode2()
+        { 
             var httpClient = new HttpClient();
             var result = httpClient.SendAsync(
                 new HttpRequestMessage(
